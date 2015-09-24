7af604e4c45eb6494515d9693493ce80cd6ce17d
andrea <andreacapretti85@gmail.com>
2015-09-24 20:51:13 +0200

    commit di andrea

---
+++ prova_you.bat
@@ -0,0 +1,5 @@
+SET PATH=%PATH%;C:\Program Files\ffmpeg\bin
+
+REM questo funziona
+REM python -m youtube_dl https://www.youtube.com/watch?v=tPEE9ZwTmy0
+python -m youtube_dl --extract-audio --audio-format mp3 -k https://www.youtube.com/watch?v=tPEE9ZwTmy0
