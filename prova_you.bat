95f390875e9150f0c1fcd273bbee7288ce3485f5
andrea <andreacapretti85@gmail.com>
2015-11-30 23:17:09 +0100

    modificato prova

--- prova_you.bat
+++ prova_you.bat
@@ -1,5 +1,6 @@
 SET PATH=%PATH%;C:\Program Files\ffmpeg\bin
 
 REM questo funziona
-REM python -m youtube_dl https://www.youtube.com/watch?v=tPEE9ZwTmy0
-python -m youtube_dl --extract-audio --audio-format mp3 -k https://www.youtube.com/watch?v=tPEE9ZwTmy0
+python -m youtube_dl https://www.youtube.com/watch?v=ewRjZoRtu0Y
+REM python -m https://www.youtube.com/watch?v=ewRjZoRtu0Y
+REM python -m youtube_dl --extract-audio --audio-format mp3 -k https://www.youtube.com/watch?v=tPEE9ZwTmy0
